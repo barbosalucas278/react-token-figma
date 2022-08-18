9cd4b534f0168ffc6519d23368f3a592b03a63f4
Lucas Barbosa <barbosalucasroberto@gmail.com>
2022-08-18 11:54:22 -0300

    'publicacion'

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <button className="btnTest">Boton de pruebaa</button>
-        <button className="btnTest">Boton de pruebaa 2</button>
-        <button className="btnTest">Boton de pruebaa 32</button>
-        <button className="btnTest">Boton de pruebaa 31</button>
+        <button className="btnTest2">Boton de pruebaa 2</button>
       </header>
     </div>
   );
